02f1f76f11db71493e7ee1e6c2a2c45e73660a20
monkeychen528 <53263222+monkeychen528@users.noreply.github.com>
2021-11-25 21:25:16 +0800

    更新聊天室功能

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -9,10 +9,10 @@ const Comment = () => {
   const [connect, setConnect] = useState(false);
   const [room, setRoom] = useState('hole');
   const [name, setName] = useState('');
-  const msg = document.querySelector('#msg');
+  const msg = useRef(null);
   const user = useRef(null);
-  // const port = 'https://ramen-chatroom.herokuapp.com';
-  const port = 'localhost:3080';
+  const port = 'https://ramen-chatroom.herokuapp.com';
+  // const port = 'localhost:3080';
   // process.env.NODE_ENV === 'production' ?: 'localhost:3050';
   const connectWebSocket = () => {
     // 伺服器目前是local所以別台電腦無法連接
@@ -34,12 +34,12 @@ const Comment = () => {
 
   // 監聽送回的訊息
   const initWebSocket = () => {
-    const showText = document.querySelector('#textBlock');
     // const 放裡面是要讓每次接收訊息時重新建新的
     ws.on('getMessage', (getMsg) => {
-      console.log(getMsg, 'this is getMsg');
+      const showText = document.querySelector('#textBlock');
+      console.log(showText);
       const block = document.createElement('p');
-      block.innerHTML = getMsg;
+      block.innerHTML = `${name} : ${getMsg}`;
       showText.append(block);
     });
   };
@@ -47,10 +47,10 @@ const Comment = () => {
   // 送出訊息到websocket
   const sendMessage = () => {
     // 清空textarea 恢復原本高度
-    console.log(msg.value);
-    ws.emit('message', { id: room, msg: msg.value });
-    msg.style.height = '41px';
-    msg.value = '';
+    console.log(msg.current.value);
+    ws.emit('message', { id: room, msg: msg.current.value });
+    msg.current.style.height = '41px';
+    msg.current.value = '';
   };
 
   // 更換房間
@@ -62,20 +62,26 @@ const Comment = () => {
     setRoom(roomName);
   };
   // 暱稱輸入、送出處理
-  // const handleName = (e) => {
-  //   e.persist();
-  //   setName(() => e.target.value);
+  const handleName = (e) => {
+    // e.persist();
+    const userLabelText = document.querySelector('.user_label_text');
+    userLabelText.style.opacity = 0;
+  };
+  // const nameCheck = () => {
+  //   if(connect)
   // };
-  const nameCheck = () => {
+  const controlPlaceHolder = () => {
+    const userLabelText = document.querySelector('.user_label_text');
+    userLabelText.style.opacity = '';
+    userLabelText.innerText = `暱稱:${user.current.value}` || '您的暱稱是?';
+    setName(user.current.value);
     connectWebSocket();
   };
-
+  const noOnline = () => ws.emit('disconnected');
   // 畫面進入的時候重新連接socket
   useEffect(() => {
-    if (connect) {
-      initWebSocket();
-      setName(user.current.value);
-    }
+    // if (connect) {
+    // }
     if (ws) {
       ws.on('connect_error', (err) => {
         if (err) {
@@ -85,15 +91,19 @@ const Comment = () => {
         }
         // return setConnect(true);
       });
+
+      ws.on('connect', () => {
+        initWebSocket();
+      });
     }
     // else {
     //   connectWebSocket();
     // }
     // 監聽如果離開頁面則發送disconnect
-    // window.addEventListener('popstate', disconnect);
+    window.addEventListener('popstate', noOnline);
     return () => {
       console.log('remove event');
-      // window.removeEventListener('popstate', disconnect);
+      window.removeEventListener('popstate', noOnline);
     };
   }, [ws]);
 
@@ -103,7 +113,7 @@ const Comment = () => {
       sendMessage();
       return;
     }
-    const grow = Math.ceil(msg.clientHeight);
+    const grow = Math.ceil(msg.current.clientHeight);
     const checkHeight = e.target.scrollHeight;
     // 拆開寫,grow取得網頁中的高度，checkoutHeight是換行時就會出現scrollHeight
     // css樣式內部高度預設35px，如果出現scrollHeight代表換行，把text高度設為一樣scroll高度
@@ -119,19 +129,25 @@ const Comment = () => {
     <>
       <Navbar />
       <div className="roomBody">
-        <div className="nameBg">
-          <div className="name_input">
-            <input
-              type="text"
-              id="user"
-              placeholder="請輸入您的暱稱"
-              // onChange={handleName}
-              ref={user}
-            // value={name}
-            />
-            <button type="button" onClick={nameCheck}>送出</button>
+        {name ? null : (
+          <div className="nameBg">
+            <div className="name_input">
+              <label htmlFor="user" className="user_label">
+                <input
+                  type="text"
+                  id="user"
+                  placeholder=""
+                  onChange={handleName}
+                  onBlur={controlPlaceHolder}
+                  ref={user}
+                // value={name}
+                />
+                <span className="user_label_text">您的暱稱是?</span>
+              </label>
+              <button type="button" disabled={!connect}>送出</button>
+            </div>
           </div>
-        </div>
+        )}
         {name && (
           <div className="roomContainer" style={{ display: 'flex' }}>
             <aside>
@@ -166,6 +182,7 @@ const Comment = () => {
                   placeholder="輸入文字"
                   id="msg"
                   onKeyDown={adjustHeight}
+                  ref={msg}
                 />
                 <input type="button" onClick={sendMessage} value="送出" />
               </div>
